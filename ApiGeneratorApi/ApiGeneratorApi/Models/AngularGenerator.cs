1a859c28f9eae0e612bafb3900c97bd40c59e929
Kyle Barton <kbarton@incomm.com>
2015-12-10 13:48:41 -0800

    Dummy work on the AngularGenerator

--- ApiGeneratorApi/ApiGeneratorApi/Models/AngularGenerator.cs
+++ ApiGeneratorApi/ApiGeneratorApi/Models/AngularGenerator.cs
@@ -12,12 +12,22 @@ namespace ApiGeneratorApi.Models
         public AngularGenerator(EndpointSpec apiSpecification, string modelType)
         {
             _modelType = modelType;
-            _outputDirectory = ConfigurationManager.AppSettings["OutputFolder"];
+            _outputDirectory = String.Format(@"{0}\{1}", ConfigurationManager.AppSettings["OutputFolder"], "js");
         }
 
         public void Generate()
         {
+            //Create the Angular App
+            File.Create(String.Format(@"{0}\{1}App.js", _outputDirectory, _modelType));
+            //Create the Angular Service
+            File.Create(String.Format(@"{0}\{1}Service.js", _outputDirectory, _modelType));
+            //Create the Angular Controller
             File.Create(String.Format(@"{0}\{1}Controller.js", _outputDirectory, _modelType));
+            //Create Angular Directives
+            File.Create(String.Format(@"{0}\{1}Directives.js", _outputDirectory, _modelType));
+            //Create the Angular Model
+            File.Create(String.Format(@"{0}\{1}.js", _outputDirectory, _modelType));
+
         }
 
     }
