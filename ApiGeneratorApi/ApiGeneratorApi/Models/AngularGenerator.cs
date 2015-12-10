86126dae060aba0d720f48a971f931b0b294222a
Kyle Barton <kbarton@incomm.com>
2015-12-10 13:39:05 -0800

    Just starting with the angular controller.

--- ApiGeneratorApi/ApiGeneratorApi/Models/AngularGenerator.cs
+++ ApiGeneratorApi/ApiGeneratorApi/Models/AngularGenerator.cs
@@ -1,14 +1,23 @@
+using System;
+using System.Configuration;
+using System.IO;
+
 namespace ApiGeneratorApi.Models
 {
     public  class AngularGenerator
     {
+        private string _outputDirectory;
+        private string _modelType;
+
         public AngularGenerator(EndpointSpec apiSpecification, string modelType)
         {
+            _modelType = modelType;
+            _outputDirectory = ConfigurationManager.AppSettings["OutputFolder"];
         }
 
         public void Generate()
         {
-
+            File.Create(String.Format(@"{0}\{1}Controller.js", _outputDirectory, _modelType));
         }
 
     }
