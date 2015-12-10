78c4a974b97a03fedaaf084531b5a48bfff7b1b5
unknown <pfinn@incomm.com>
2015-12-10 14:21:02 -0800

    made some changes for alex

--- ApiGeneratorApi/ApiGeneratorApi/Models/BusinessLogicGenerator.cs
+++ ApiGeneratorApi/ApiGeneratorApi/Models/BusinessLogicGenerator.cs
@@ -10,18 +10,14 @@ namespace ApiGeneratorApi.Models
         private string _data;
 
         public readonly string FilePath;
+        private readonly string _modelType;
         private const string ClassSuffix = "BusinessLogic";
 
-        public BusinessLogicGenerator(EndpointSpec apiSpecification)
+        public BusinessLogicGenerator(EndpointSpec apiSpecification, string modelType)
         {
             _apiSpecification = apiSpecification;
-
-            FilePath = string.Format("{0}/BusinessLogic/{1}", WebConfigurationManager.AppSettings["PFUserName"], GetType());
-        }
-
-        public new string GetType()
-        {
-            return _apiSpecification.Uri;
+            _modelType = modelType;
+            FilePath = string.Format("{0}/BusinessLogic/{1}", WebConfigurationManager.AppSettings["OutputFolder"], _modelType);
         }
 
         public void Generate()
@@ -61,8 +57,8 @@ namespace ApiGeneratorApi.Models
         private string CompileObject(bool isConcrete)
         {
             var sb = new StringBuilder();
-            sb.AppendFormat("   public {0}{1}{2}", isConcrete? "class " : "interface i", GetType(), ClassSuffix);
-            sb.AppendLine(isConcrete? string.Format(" : I{0}{1}", GetType(), ClassSuffix): ""); //Inherit from interface if isConcrete
+            sb.AppendFormat("   public {0}{1}{2}", isConcrete? "class " : "interface i", _modelType, ClassSuffix);
+            sb.AppendLine(isConcrete? string.Format(" : I{0}{1}", _modelType, ClassSuffix): ""); //Inherit from interface if isConcrete
             sb.AppendLine();
             sb.AppendLine("{");
             sb.AppendLine(CompileProperties(isConcrete));
@@ -82,17 +78,17 @@ namespace ApiGeneratorApi.Models
         private string CompileProperties(bool isConcrete)
         {
             var sb = new StringBuilder();
-            sb.AppendFormat(" {0}readonly I{1}Reader _reader;", isConcrete ? "private " : "", GetType());
-            sb.AppendFormat(" {0}readonly I{1}Writer _writer;", isConcrete ? "private " : "", GetType());
+            sb.AppendFormat(" {0}readonly I{1}Reader _reader;", isConcrete ? "private " : "", _modelType);
+            sb.AppendFormat(" {0}readonly I{1}Writer _writer;", isConcrete ? "private " : "", _modelType);
             return sb.ToString();
         }
 
         private string CompileConstrutors()
         {
             var sb = new StringBuilder();
-            sb.AppendFormat("       public {0}{1}() : this(new {0}Reader(), new {0}Writer())", GetType(), ClassSuffix);
+            sb.AppendFormat("       public {0}{1}() : this(new {0}Reader(), new {0}Writer())", _modelType, ClassSuffix);
             sb.AppendLine("{}");
-            sb.AppendFormat("       public {0}{1}() (I{0}Reader reader, I{0}Writer writer)", GetType(), ClassSuffix);
+            sb.AppendFormat("       public {0}{1}() (I{0}Reader reader, I{0}Writer writer)", _modelType, ClassSuffix);
             sb.AppendLine(" {");
             sb.AppendLine("         _reader = reader;");
             sb.AppendLine("         _writer = writer;");
@@ -103,7 +99,7 @@ namespace ApiGeneratorApi.Models
         private string CompileWriteFunction(bool isConcrete)
         {
             var sb = new StringBuilder();
-            sb.AppendFormat("       {0}int Write({1} data)", isConcrete? "public " : "", GetType());
+            sb.AppendFormat("       {0}int Write({1} data)", isConcrete? "public " : "", _modelType);
             sb.AppendLine("{");
             sb.AppendLine("             return _writer.Write(data);");
             sb.AppendLine("}");
@@ -113,7 +109,7 @@ namespace ApiGeneratorApi.Models
         private string CompileReadFunction(bool isConcrete)
         {
             var sb = new StringBuilder();
-            sb.AppendFormat("       {0}{1} Get(int Id)", isConcrete ? "public " : "", GetType());
+            sb.AppendFormat("       {0}{1} Get(int Id)", isConcrete ? "public " : "", _modelType);
             sb.AppendLine("{");
             sb.AppendLine("             return _reder.Get(Id);");
             sb.AppendLine("}");
@@ -123,7 +119,7 @@ namespace ApiGeneratorApi.Models
         private string CompileReadAllFunction(bool isConcrete)
         {
             var sb = new StringBuilder();
-            sb.AppendFormat("       {0}List<{1}> GetAll()", isConcrete ? "public " : "", GetType());
+            sb.AppendFormat("       {0}List<{1}> GetAll()", isConcrete ? "public " : "", _modelType);
             sb.AppendLine("{");
             sb.AppendLine("             return _reder.GetAll();");
             sb.AppendLine("}");
