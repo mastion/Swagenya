d949bb9da17be1731e606f8431b67ffd19bd1734
unknown <pfinn@incomm.com>
2015-12-10 15:43:41 -0800

    can generate the files correctly

--- ApiGeneratorApi/ApiGeneratorApi/Models/DataAccessGenerator.cs
+++ ApiGeneratorApi/ApiGeneratorApi/Models/DataAccessGenerator.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text;
-using System.Web.Configuration;
 using ApiGeneratorApi.Util;
 
 namespace ApiGeneratorApi.Models
@@ -8,48 +7,49 @@ namespace ApiGeneratorApi.Models
     {
         private EndpointSpec _apiSpecification;
         private string _modelType;
-        public readonly string FilePath;
 
         public DataAccessGenerator(EndpointSpec apiSpecification, string modelType)
         {
             _apiSpecification = apiSpecification;
             _modelType = modelType;
-            FilePath = string.Format("{0}/DataAccess/{1}", WebConfigurationManager.AppSettings["OutputFolder"], _modelType);
+
         }
 
         public void Generate()
         {
-            new FileWriter().WriteFile(FilePath, GenerateReader());
-            new FileWriter().WriteFile(FilePath, GenerateWriter());
+            var filePath = string.Format("{0}/{1}Reader.cs", new FolderWriter().GetFolderName("DataAccess"), _modelType);
+            new FileWriter().WriteFile(filePath, GenerateReader());
+             filePath = string.Format("{0}/{1}Writer.cs", new FolderWriter().GetFolderName("DataAccess"), _modelType);
+            new FileWriter().WriteFile(filePath, GenerateWriter());
         }
 
         private string GenerateWriter()
         {
             var sb = new StringBuilder();
-            sb.Append("using System.Data;");
-            sb.Append("using System.Linq;");
-            sb.Append("using Dapper;");
-            sb.Append("using Giftango.Component.Utility;");
-            sb.Append("namespace Giftango.Domain.Reader");
-            sb.Append("{");
-                sb.Append(string.Format("public class {0}Writer", _modelType));
-                sb.Append("{");
+            sb.AppendLine("using System.Data;");
+            sb.AppendLine("using System.Linq;");
+            sb.AppendLine("using Dapper;");
+            sb.AppendLine("using Giftango.Component.Utility;");
+            sb.AppendLine("namespace Giftango.Domain.Reader");
+            sb.AppendLine("{");
+                sb.AppendFormat("public class {0}Writer", _modelType);
+                sb.AppendLine("{");
                     sb.AppendFormat("public int Write({0} toWrite)", _modelType);
-                    sb.Append("{");
-                        sb.Append("using (var connection = ConnectionHelper.GetConnection())");
-                        sb.Append("{");
+                    sb.AppendLine("{");
+                        sb.AppendLine("using (var connection = ConnectionHelper.GetConnection())");
+                        sb.AppendLine("{");
                             sb.AppendFormat("return connection.Query<int>(\"[dbo].[Insert{0}]\"", _modelType).AppendLine("new { toWrite }, commandType: CommandType.StoredProcedure);");
-                        sb.Append("}");
-                    sb.Append("}");
+                        sb.AppendLine("}");
+                    sb.AppendLine("}");
                     sb.AppendFormat("public int WriteById(int id, {0} toWrite)", _modelType);
-                    sb.Append("{");
-                        sb.Append("using (var connection = ConnectionHelper.GetConnection())");
-                        sb.Append("{");
-                            sb.AppendFormat("return connection.Query<int}>(\"[dbo].[Get{0}]\"",_modelType).AppendLine("new {id, toWrite}, commandType: CommandType.StoredProcedure);");
-                        sb.Append("}");
-                    sb.Append("}");
-                sb.Append("}");
-            sb.Append("}");
+                    sb.AppendLine("{");
+                        sb.AppendLine("using (var connection = ConnectionHelper.GetConnection())");
+                        sb.AppendLine("{");
+                            sb.AppendFormat("return connection.Query<int>(\"[dbo].[Get{0}]\"",_modelType).AppendLine("new {id, toWrite}, commandType: CommandType.StoredProcedure);");
+                        sb.AppendLine("}");
+                    sb.AppendLine("}");
+                sb.AppendLine("}");
+            sb.AppendLine("}");
 
             return sb.ToString();
         }
@@ -57,30 +57,30 @@ namespace ApiGeneratorApi.Models
         private string GenerateReader()
         {
             var sb = new StringBuilder();
-            sb.Append("using System.Data;");
-            sb.Append("using System.Linq;");
-            sb.Append("using Dapper;");
-            sb.Append("using Giftango.Component.Utility;");
-            sb.Append("namespace Giftango.Domain.Reader");
-            sb.Append("{");
-                sb.Append(string.Format("public class {0}Reader", _modelType));
-                sb.Append("{");
-                    sb.Append(string.Format("public List<{0}> GetAll()", _modelType));
-                    sb.Append("{");
-                        sb.Append("using (var connection = ConnectionHelper.GetConnection())");
-                        sb.Append("{");
-                            sb.Append(string.Format("return connection.Query<{0}>(\"[dbo].[GetAll{0}]\", commandType: CommandType.StoredProcedure);", _modelType));
-                        sb.Append("}");
-                    sb.Append("}");
-                    sb.Append(string.Format("public {0} GetById(int id)", _modelType));
-                    sb.Append("{");
-                        sb.Append("using (var connection = ConnectionHelper.GetConnection())");
-                        sb.Append("{");
-                            sb.Append(string.Format("return connection.Query<{0}>(\"[dbo].[Get{0}]\", new {1}, commandType: CommandType.StoredProcedure);", _modelType, "{id}"));
-                        sb.Append("}");
-                    sb.Append("}");
-                sb.Append("}");
-            sb.Append("}");
+            sb.AppendLine("using System.Data;");
+            sb.AppendLine("using System.Linq;");
+            sb.AppendLine("using Dapper;");
+            sb.AppendLine("using Giftango.Component.Utility;");
+            sb.AppendLine("namespace Giftango.Domain.Reader");
+            sb.AppendLine("{");
+                sb.AppendFormat("public class {0}Reader", _modelType).AppendLine();
+                sb.AppendLine("{");
+                    sb.AppendFormat("public List<{0}> GetAll()", _modelType).AppendLine();
+                    sb.AppendLine("{");
+                        sb.AppendLine("using (var connection = ConnectionHelper.GetConnection())");
+                        sb.AppendLine("{");
+                            sb.AppendFormat("return connection.Query<{0}>(\"[dbo].[GetAll{0}]\", commandType: CommandType.StoredProcedure);", _modelType).AppendLine();
+                        sb.AppendLine("}");
+                    sb.AppendLine("}");
+                    sb.AppendFormat("public {0} GetById(int id)", _modelType).AppendLine();
+                    sb.AppendLine("{");
+                        sb.AppendLine("using (var connection = ConnectionHelper.GetConnection())");
+                        sb.AppendLine("{");
+                            sb.AppendFormat("return connection.Query<{0}>(\"[dbo].[Get{0}]\", new {1}, commandType: CommandType.StoredProcedure);", _modelType, "{id}");
+                        sb.AppendLine("}");
+                    sb.AppendLine("}");
+                sb.AppendLine("}");
+            sb.AppendLine("}");
 
             return sb.ToString();
         }
