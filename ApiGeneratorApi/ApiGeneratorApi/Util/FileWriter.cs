cef4732799df2d34074f1cc8e3d7b7e5011b515a
Kyle Barton <kbarton@incomm.com>
2015-12-10 15:33:49 -0800

    We a line.

--- ApiGeneratorApi/ApiGeneratorApi/Util/FileWriter.cs
+++ ApiGeneratorApi/ApiGeneratorApi/Util/FileWriter.cs
@@ -11,7 +11,6 @@ namespace ApiGeneratorApi.Util
     {
         public void WriteFile(string filePath, string data)
         {
-            Directory.CreateDirectory(filePath.Remove(filePath.LastIndexOf('/')));
             using (var fs = new FileStream(filePath, FileMode.Create))
             using (var sw = new StreamWriter(fs))
                 sw.Write(data);
